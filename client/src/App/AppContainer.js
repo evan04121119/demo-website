3d713846d884dc79bd0584f42ae9361a894a2d7b
BerbyL <jinbo.liu@outlook.com>
2021-07-03 23:27:09 +0200

    first container

---
+++ client/src/App/AppContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class AppContainer extends Component {
+  componentDidMount() {
+
+  }
+  render() {
+    return (<text>test !!!</text>)
+  }
+}
+
+export default AppContainer;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppContainer from './App/AppContainer';
+
+// not useless
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
